bf5e265f438b8be1f03fbc5d4c2774922d621645
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2015-02-04 16:26:16 -0500

    line to create the eigenvectors

--- data/tmp_create_data.sh
+++ data/tmp_create_data.sh
@@ -3,3 +3,4 @@
 
 ./smooth_bolshoi.x /home/forero/work/data/ELVIS/fullboxes/GVD_F09_1001_000_z125_box14_009 -n 128
 ./smooth_bolshoi.x -d GVD_F09_1001_000_z125_box14_009 -s 2.0
+./smooth_bolshoi.x -d GVD_F09_1001_000_z125_box14_009 -s 2.0 -eigenvector
