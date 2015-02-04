8de545bc90ccd2eb8d1646930e10f01406ecae8a
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2015-02-04 16:24:58 -0500

    how was the data created

---
+++ data/tmp_create_data.sh
@@ -0,0 +1,5 @@
+# This is how the first set of data was created on bachue. 
+# This has to be replaced by a Makefile.
+
+./smooth_bolshoi.x /home/forero/work/data/ELVIS/fullboxes/GVD_F09_1001_000_z125_box14_009 -n 128
+./smooth_bolshoi.x -d GVD_F09_1001_000_z125_box14_009 -s 2.0
